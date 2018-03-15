1933ce349ac2441377fd7f89cb9cc819e0a22b94
Jakub Puchała <puchala.czwa@gmail.com>
2018-03-15 07:06:49 +0100

    1. Some minor fixes

--- Musoq.Console/Helpers/Configuration.cs
+++ Musoq.Console/Helpers/Configuration.cs
@@ -1,7 +1,9 @@
-﻿namespace Musoq.Console.Helpers
+﻿using System.Configuration;
+
+namespace Musoq.Console.Helpers
 {
     public static class Configuration
     {
-        public static string Address => "localhost:9001";
+        public static string Address => ConfigurationManager.AppSettings["ApiAddress"];
     }
 }
--- Musoq.Service/ContextService.Designer.cs
+++ Musoq.Service/ContextService.Designer.cs
@@ -28,8 +28,11 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
-            this.ServiceName = "Service1";
+            // 
+            // ContextService
+            // 
+            this.ServiceName = "Musoq.QueryEngine";
+
         }
 
         #endregion
