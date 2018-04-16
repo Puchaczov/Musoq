102fec91848806b8db26f9bc2bdf504042e29290
Jakub Puchała <puchala.czwa@gmail.com>
2018-04-16 17:10:42 +0200

    1. Added transpiler branch

--- Musoq.Converter/InstanceCreator.cs
+++ Musoq.Converter/InstanceCreator.cs
@@ -1,4 +1,10 @@
-﻿using Musoq.Evaluator;
+﻿using System;
+using System.IO;
+using System.Reflection;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Musoq.Evaluator;
+using Musoq.Evaluator.Tables;
 using Musoq.Evaluator.Visitors;
 using Musoq.Parser;
 using Musoq.Parser.Lexing;
@@ -29,12 +35,70 @@ namespace Musoq.Converter
 
             var codeGenerator = new CodeGenerationVisitor(schemaProvider, metadataCreator.TableMetadata);
             var traverser =
-                new CodeGenerationTraverseVisitor(codeGenerator, metadataCreator.AggregationMethods.AsReadOnly());
+                new CodeGenerationTraverseVisitor(codeGenerator);
 
             query.Accept(traverser);
 
             return codeGenerator.VirtualMachine;
         }
+
+        public static Func<Table> CreateCompiled(RootNode root, ISchemaProvider schemaProvider)
+        {
+            schemaProvider = new TransitionSchemaProvider(schemaProvider);
+
+            var query = root;
+
+            var rewriter = new RewriteTreeVisitor((TransitionSchemaProvider)schemaProvider);
+            var rewriteTraverser = new RewriteTreeTraverseVisitor(rewriter);
+
+            query.Accept(rewriteTraverser);
+
+            query = rewriter.RootScript;
+
+            var metadataCreator = new PreGenerationVisitor();
+            var metadataTraverser = new PreGenerationTraverseVisitor(metadataCreator);
+
+            query.Accept(metadataTraverser);
+
+            var codeGenerator = new RuntimeGeneratorVisitor();
+            var traverser =
+                new CodeGenerationTraverseVisitor(codeGenerator);
+
+            query.Accept(traverser);
+
+            var syntaxTree = CSharpSyntaxTree.ParseText(codeGenerator.Script);
+
+            var compilation = CSharpCompilation.Create(
+                "assemblyName",
+                new[] { syntaxTree },
+                new[] { MetadataReference.CreateFromFile(typeof(object).Assembly.Location) },
+                new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary));
+
+            using (var dllStream = new MemoryStream())
+            using (var pdbStream = new MemoryStream())
+            {
+                var emitResult = compilation.Emit(dllStream, pdbStream);
+                if (!emitResult.Success)
+                {
+                    throw new NotSupportedException();
+                }
+
+                Assembly asm = Assembly.Load(dllStream.ToArray(), pdbStream.ToArray());
+
+                var type = asm.GetType("Musoq.CodeGenerated.TranslatedQuery");
+                object obj = Activator.CreateInstance(type);
+                var func = type.InvokeMember("ComputeQuery",
+                    BindingFlags.Default | BindingFlags.InvokeMethod,
+                    null,
+                    obj,
+                    new object[0]);
+
+                return ((Func<Table>)func);
+            }
+
+            throw new NotImplementedException();
+        }
+
 
         public static VirtualMachine Create(string script, ISchemaProvider schemaProvider)
         {
---
+++ Musoq.Evaluator/RuntimeScripts/RuntimeExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace Musoq.Evaluator.RuntimeScripts
+{
+    internal static class T4Extensions
+    {
+        public static MethodInfo GetMethod(this Type type, string method, params Type[] parameters)
+        {
+            return type.GetRuntimeMethod(method, parameters);
+        }
+    }
+}
+
+namespace System.CodeDom.Compiler
+{
+    public class CompilerErrorCollection : List<CompilerError>
+    {
+    }
+
+    public class CompilerError
+    {
+        public string ErrorText { get; set; }
+
+        public bool IsWarning { get; set; }
+    }
+}
