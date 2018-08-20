df02b37e1be587e0297c0cb5e7fa5cedbadd7503
Jakub Puchała <puchala.czwa@gmail.com>
2018-08-20 15:07:34 +0200

    1. Added realeased copy project. 2. Set all plugins and Musoq.Converter project to be copied automatically to some specific path if _conf.release.mrf exists and has valid destination path.

---
+++ Musoq.Tools.CopyReleased/ApplicationArguments.cs
@@ -0,0 +1,19 @@
+﻿using CommandLine;
+
+namespace Musoq.Tools.CopyReleased
+{
+    public class ApplicationArguments
+    {
+        [Option("source", HelpText = "Source release folder.", Required = true)]
+        public string SourceDir { get; set; }
+
+        [Option("destinationDirName", HelpText = "Source destination folder name.", Required = true)]
+        public string DestinationDirName { get; set; }
+
+        [Option("configurationDir", HelpText = "Source destination folder name.", Required = true)]
+        public string ConfigurationDir { get; set; }
+
+        [Option("buildType", HelpText = "DEBUG / RELEASE", Required = true)]
+        public string BuildType { get; set; }
+    }
+}
