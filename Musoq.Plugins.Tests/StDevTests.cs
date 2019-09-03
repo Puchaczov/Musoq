863cb1b6da87e4de429658517cf826e51458b9f3
Jakub Puchała <puchala.czwa@gmail.com>
2019-09-03 07:51:26 +0200

    1. Added StDev aggregation method, however without estimator load.

---
+++ Musoq.Plugins.Tests/StDevTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Musoq.Plugins.Tests
+{
+    [TestClass]
+    public class StDevTests : LibraryBaseBaseTests
+    {
+
+        [TestMethod]
+        public void StdDevTest()
+        {
+            Library.SetStDev(Group, "test", 60000, 0);
+            Library.SetStDev(Group, "test", 80000, 0);
+
+            Assert.IsTrue(0.01m > (Library.StDev(Group, "test") - 14142.13m));
+        }
+
+        [TestMethod]
+        public void StdDevTest_2()
+        {
+            Library.SetStDev(Group, "test", 5, 0);
+            Library.SetStDev(Group, "test", 6, 0);
+            Library.SetStDev(Group, "test", 8, 0);
+            Library.SetStDev(Group, "test", 9, 0);
+
+            Assert.IsTrue(0.001m > (Library.StDev(Group, "test") - 1.8257m));
+        }
+    }
+}
---
+++ Musoq.Plugins/Lib/LibraryBaseStDev.cs
@@ -0,0 +1,42 @@
+﻿using Musoq.Plugins.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Musoq.Plugins
+{
+    public partial class LibraryBase
+    {
+        [AggregationSetMethod]
+        public void SetStDev([InjectGroup] Group group, string name, decimal? value, int parent = 0)
+        {
+            SetAvg(group, $"{name}:avgStdDev", value, parent);
+            var parentGroup = GetParentGroup(group, parent);
+
+            if (!value.HasValue)
+                return;
+
+            var list = parentGroup.GetOrCreateValue($"{name}:itemsStdDev", () => new List<double>());
+
+            list.Add((double)value.Value);
+        }
+
+        [AggregationGetMethod]
+        public decimal StDev([InjectGroup] Group group, string name)
+        {
+            var avg = (double)Avg(group, $"{name}:avgStdDev");
+            var parentGroup = GetParentGroup(group, 0);
+            var values = parentGroup.GetValue<List<double>>($"{name}:itemsStdDev");
+
+            double sum = 0;
+            for (int i = 0; i < values.Count; ++i)
+            {
+                sum += Math.Pow((values[i] - avg), 2);
+            }
+
+            var variance = sum / (values.Count - 1);
+
+            return (decimal)Math.Sqrt(variance);
+        }
+    }
+}
