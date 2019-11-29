72225f97239c977ec2ebd911648a1d719d953462
Jakub Puchała <puchala.czwa@gmail.com>
2019-11-29 18:52:44 +0100

    1. Modified publish script.

--- Publish.Nuget.ps1
+++ Publish.Nuget.ps1
@@ -1,11 +1,16 @@
-﻿param([String]$project, [String]$apiKey)
+﻿param([String] $nuget, [String]$project, [String]$apiKey)
 
 $fileName=Get-ChildItem -Path $path -File | Select-Object -First 1 | Select -exp Name
-$text = Nuget.exe list Puchaczov | Select-String -Pattern $project | Out-String
+$text = Invoke-Expression "$nuget list Puchaczov" | Select-String -Pattern $project | Out-String
 $name,$version = $text.trim().split(" ")
 $publishedFileName = "$name.$version.nupkg"
 
 if ($fileName -ne $publishedFileName){
-	echo $fileName, $publishedFileName
-	dotnet push "./$fileName" -Source https://api.nuget.org/v3/index.json -ApiKey $apiKey
+	echo "Publishing $publishedFileName...";
+	Invoke-Expression "$nuget push './$fileName' -Source https://api.nuget.org/v3/index.json -ApiKey $apiKey"
+	echo "done."
+}
+else
+{
+	echo "File $publishedFileName has already been published. Skipping."
 }
