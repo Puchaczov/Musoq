23d387f496271d0c9f43b36b86927c4598f0ace9
Jakub Puchała <puchaczov.czwa@gmail.com>
2019-11-29 19:25:43 +0100

    Feature/nuget deploy (#12)
    1. Implemented automatic nuget packages automatic publish after merge to master.

---
+++ Publish.Nuget.ps1
@@ -0,0 +1,16 @@
+﻿param([String] $nuget, [String]$project, [String]$apiKey)
+
+$fileName=Get-ChildItem -Path $path -File | Select-Object -First 1 | Select -exp Name
+$text = Invoke-Expression "$nuget list Puchaczov" | Select-String -Pattern $project | Out-String
+$name,$version = $text.trim().split(" ")
+$publishedFileName = "$name.$version.nupkg"
+
+if ($fileName -ne $publishedFileName){
+	echo "Publishing $fileName...";
+	Invoke-Expression "$nuget push './$fileName' -Source https://api.nuget.org/v3/index.json -ApiKey $apiKey"
+	echo "done."
+}
+else
+{
+	echo "File $publishedFileName has already been published. Skipping."
+}
