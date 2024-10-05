75c121aa3a89def377f797d1d5c68d60b0c96393
puchacz <puchala.czwa@gmail.com>
2024-10-05 11:28:49 +0200

    Initial preparation for loading heavy dependencies

--- Musoq.Schema/ISchema.cs
+++ Musoq.Schema/ISchema.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Reflection;
+using System.Threading.Tasks;
 using Musoq.Schema.DataSources;
+using Musoq.Schema.Reflection;
 
 namespace Musoq.Schema;
 
@@ -12,17 +14,23 @@ public interface ISchema
 
     RowSource GetRowSource(string name, RuntimeContext runtimeContext, params object[] parameters);
 
-    Reflection.SchemaMethodInfo[] GetConstructors(string methodName);
+    SchemaMethodInfo[] GetConstructors(string methodName);
 
-    Reflection.SchemaMethodInfo[] GetConstructors();
+    SchemaMethodInfo[] GetConstructors();
 
-    Reflection.SchemaMethodInfo[] GetRawConstructors();
+    SchemaMethodInfo[] GetRawConstructors();
 
-    Reflection.SchemaMethodInfo[] GetRawConstructors(string methodName);
+    SchemaMethodInfo[] GetRawConstructors(string methodName);
 
     bool TryResolveMethod(string method, Type[] parameters, Type entityType, out MethodInfo methodInfo);
 
     bool TryResolveRawMethod(string method, Type[] parameters, out MethodInfo methodInfo);
 
     bool TryResolveAggregationMethod(string method, Type[] parameters, Type entityType, out MethodInfo methodInfo);
+
+    static virtual Task LoadRequiredDependenciesAsync()
+    {
+        // The default implementation does nothing and it is intended as most of the schemas won't require any additional dependencies.
+        return Task.CompletedTask;
+    }
 }
